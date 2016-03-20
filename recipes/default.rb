fbdad784a1aee001a7014c496f8d15beb54352d7
Bowen Sun <bowensun@gmail.com>
2016-03-20 18:07:45 +0000

    simplify db password lookup

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,11 +8,8 @@
 #
 include_recipe "freeradius::#{node[:freeradius][:install_method]}"
 
-user_auth = Chef::EncryptedDataBagItem.load(node['freeradius']['db_databag'], node['freeradius']['db_databag_item'])
-user_auth.each do |user, passwd|
-  node.override['freeradius']['db_login'] = user
-  node.override['freeradius']['db_password'] = passwd
-end
+user_auth = EncryptedPasswords.new(node, node['freeradius']['db_databag_item'])
+node.override['freeradius']['db_password'] = user_auth.find_password(node['freeradius']['db_databag_item'], node['freeradius']['db_login'])
 
 if node['freeradius']['enable_ldap'] == true
   include_recipe "freeradius::ldap"

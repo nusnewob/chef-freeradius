c54b8b4bc2f96ee0f64b14c14d7f28af030d2de6
Bowen Sun <bowensun@gmail.com>
2016-05-13 11:32:21 +0100

    enable counter by default

--- recipes/default.rb
+++ recipes/default.rb
@@ -29,6 +29,18 @@ service node['freeradius']['service'] do
   action [:enable, :start]
 end
 
+template "#{node['freeradius']['dir']}/mods-available/counter" do
+  source "mod-counter.erb"
+  owner node['freeradius']['user']
+  group node['freeradius']['group']
+  mode 0600
+end
+
+link "#{node['freeradius']['dir']}/mods-enabled/counter" do
+  to "#{node['freeradius']['dir']}/mods-available/counter"
+  notifies :restart, "service[#{node['freeradius']['service']}]", :immediately
+end
+
 if node['freeradius']['enable_ldap'] == true
   include_recipe "freeradius::ldap"
 end
